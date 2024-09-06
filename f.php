8f76c1843ba6cd7f8c13cb5b2fab9f07ed063fd7
roshni-verma <121926627+roshni-verma@users.noreply.github.com>
2024-09-06 12:48:21 +0530

    Add files via upload

---
+++ f.php
@@ -0,0 +1,27 @@
+<html> 
+<head>
+<title>insert</title>
+</head>
+<body>
+<div class="row text-center">
+ <div class="container">
+ <h1>INSERT DATA</h1>
+ <fome action="index.php" method="post">
+ <input type="text" name="firstname" placeholder="firstname"><br><br>
+ <input type="text" name="lastname" placeholder="lastname"><br><br>
+ <input type="gmail" name="gmail" placeholder="gmail"><br><br>
+ <input type "text" name="number" placeholder="number"><br><br>
+ <input type "text" name "address" placeholder="address"><br><br>
+ <input type="submit" name="submit" value="insert"><br><br>
+</form>
+<button><a href="show.php">show data</a></button>
+</div>
+ </div>
+ </body>
+ </htmL>
+ <?php
+ error_reporting(0);
+ include 'conection.php';
+ if(isset($__post
+
+ 
